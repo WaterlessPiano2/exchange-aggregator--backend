3b1ea9c7742538306cb2e79085e96eb03c3e34d2
salih <scderya@gmail.com>
2022-12-27 22:12:57 +0000

    made login endpoint, that checks email and password and returns a jwt token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { JwtService } from '@nestjs/jwt';
 import { AuthDto } from './dto/auth.dto';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
@@ -21,6 +21,17 @@ export class AuthService {
     const user = await newUser.save();
     return this.createToken(user.email);
   }
+
+  async login(dto: AuthDto) {
+    const user = await this.userModel.findOne({ email: dto.email });
+    if (!user) throw new UnauthorizedException('Wrong email or password.');
+    const isPasswordCorrect = await bcrypt.compare(dto.password, user.password);
+    if (!isPasswordCorrect)
+      throw new UnauthorizedException('Wrong email or password.');
+
+    return this.createToken(user.email);
+  }
+
   createToken(email: string) {
     return this.JwtService.sign({ email });
   }
