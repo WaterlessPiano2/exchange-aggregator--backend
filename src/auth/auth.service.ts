4175ce79f3b775682f09920f4716ba8992699125
salih <scderya@gmail.com>
2022-12-27 20:38:56 +0000

    installed bcrypt for salted hashinng passwords saved

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,14 +3,16 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
   async register(dto: AuthDto) {
+    const saltedHashedPassword = await bcrypt.hash(dto.password, 10);
     const newUser = new this.userModel({
       email: dto.email,
-      password: dto.password,
+      password: saltedHashedPassword,
     });
     return await newUser.save();
   }
