90fe2661e13c65784782a204dbd975f260a1264a
salih <scderya@gmail.com>
2022-12-28 12:50:00 +0000

    created the module, service and controller templates, using nest js commands, for the exchanges

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ExchangesModule } from './exchanges/exchanges.module';
 
 @Module({
-  imports: [AuthModule, MongooseModule.forRoot('mongodb://localhost/NestDB')],
+  imports: [AuthModule, MongooseModule.forRoot('mongodb://localhost/NestDB'), ExchangesModule],
   controllers: [AppController],
   providers: [AppService],
 })
